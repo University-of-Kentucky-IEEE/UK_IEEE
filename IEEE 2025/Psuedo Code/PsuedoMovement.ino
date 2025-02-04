da45d11f2e83a162bd366f8e771802bb383dc929
alex-dimayuga <146902320+alex-dimayuga@users.noreply.github.com>
2025-02-04 18:27:24 -0500

    Messing around with movement... <3

--- IEEE 2025/Psuedo Code/PsuedoMovement.ino
+++ IEEE 2025/Psuedo Code/PsuedoMovement.ino
@@ -1,1 +1,17 @@
 // PsuedoCode for movement
+#include "Basic Movement.h"
+
+void MoveForward() {
+    FL.writeMicroseconds(MinPulse);
+    BL.writeMicroseconds(MaxPulse);
+
+    FR.writeMicroseconds(MaxPulse);
+    BR.writeMicroseconds(MinPulse);
+}
+// Move forward  | FROM START
+// Turn clockwise 90 deg | FACING CAVE
+// Move forward | TOWARDS CAVE ENTRANCE
+// Stop | AT BACK OF CAVE (IN MIDDLE)
+// Turn clockwise 90 deg | FACING TOP RIGHT CORNER OF CAVE
+// Move backward | UNTIL HITTING BACK RIGHT CORNER OF CAVE
+// Move forward |
