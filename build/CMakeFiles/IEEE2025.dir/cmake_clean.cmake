ddff9a65c36761033860308f82417a34d1a021b1
Njones27 <nathanjjones2004@gmail.com>
2025-02-24 18:30:33 -0500

    Random junk we can ignore

---
+++ build/CMakeFiles/IEEE2025.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IEEE2025.dir/main.cpp.o"
+  "CMakeFiles/IEEE2025.dir/main.cpp.o.d"
+  "IEEE2025"
+  "IEEE2025.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IEEE2025.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
