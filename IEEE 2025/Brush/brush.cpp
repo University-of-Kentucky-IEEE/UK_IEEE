9b8d2374d9042dff0ff6c9da62cb3270df9a72b1
Njones27 <nathanjjones2004@gmail.com>
2025-03-28 14:18:40 -0400

    Updated brush code

--- IEEE 2025/Brush/brush.cpp
+++ IEEE 2025/Brush/brush.cpp
@@ -1,19 +1,19 @@
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
-int motor2pin1 = 4;
-int motor2pin2 = 5;
+//int motor2pin1 = 4;
+//int motor2pin2 = 5;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
-  pinMode(motor2pin1, OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
+  //pinMode(motor2pin1, OUTPUT);
+  //pinMode(motor2pin2, OUTPUT);
 
   //(Optional)
-  pinMode(9,  OUTPUT); 
-  pinMode(10, OUTPUT);
+  pinMode(1,  OUTPUT); 
+  //pinMode(10, OUTPUT);
   //(Optional)
 }
 
@@ -22,24 +22,24 @@ void loop() {
 
   //Controlling speed (0  = off and 255 = max speed):     
   //(Optional)
-  analogWrite(9, 100); //ENA  pin
-  analogWrite(10, 200); //ENB pin
+  analogWrite(1, 100); //ENA  pin
+  //analogWrite(10, 200); //ENB pin
   //(Optional)
   
   //Exhaust
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
 
-  digitalWrite(motor2pin1, HIGH);
+  /*digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
-  delay(3000);
+  delay(3000);*/
 
 
   //Intake
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
 
-  digitalWrite(motor2pin1, LOW);
+  /*digitalWrite(motor2pin1, LOW);
   digitalWrite(motor2pin2, HIGH);
-  delay(3000);
+  delay(3000);*/
 }
