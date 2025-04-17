85172e63390669af3997e344d2b262c8888687de
Arcanedoom <nadia.turner@uky.edu>
2025-04-17 17:18:43 -0400

    removed old vision code

--- IEEE 2025/visionSystem/src/VisionSys.cpp
+++ IEEE 2025/visionSystem/src/VisionSys.cpp
@@ -1,3 +1,8 @@
+// Use these functions in the main robot. Designed for VisionResponse to be called once the tag needs to be read
+
+// tries to get a valid result from the camera within the span of 10 seconds
+// if receives valid response, returns a value 0-4
+// if invalid, returns -1
 int VisionResponse(){
     int validTag = -1;
     const unsigned long period = 10000; // how long to wait and try to get valid value
@@ -14,80 +19,81 @@ int VisionResponse(){
     //Serial.println("failed to read tag"); // debug, message said even when response valid
     return(validTag);
   }
+
+  // returns the msot common value from a sample of tag readings
+int ReadTag(){
+  // counters array
+  int vals[5] = {0,0,0,0,0};
+  //debug print vals
   
-  int ReadTag(){
-    // counters array
-    int vals[5] = {0,0,0,0,0};
-    //debug print vals
-    
-    // try to get a valid tag value 
-    
-    // for loop of tries to read tag
-    for(int j = 0; j < 10;j++){
-        // result from sampling
-        int tagVal = -1;
-        
-        // read numerous values from the tag and increment the array
-        for(int i = 0; i < 1000; i++){
-        // get a tag reading and add one to corresponding array index if valid value
-        int reading = DetectSingleTag();
-        if((reading > -1) && (reading < 5)){
-          vals[reading] = vals[reading] + 1;
-        //debug print vals
-          }
-      }
-          // find max value 
-          int maxIndex = -1;
-          int maxVal = 0;
-          for(int i = 0; i < 5; i++){
-              int curVal = vals[i];
-              if(curVal > maxVal){
-                  maxIndex = i;
-                  maxVal = curVal;
-              }
-          }
-          tagVal = maxIndex;
+  // try to get a valid tag value 
+  
+  // for loop of tries to read tag
+  for(int j = 0; j < 10;j++){
+      // result from sampling
+      int tagVal = -1;
       
-      // return tagVal is not -1, it is a intended result
-      if(tagVal != -1){
-          return tagVal;
-      }
+      // read numerous values from the tag and increment the array
+      for(int i = 0; i < 1000; i++){
+      // get a tag reading and add one to corresponding array index if valid value
+      int reading = DetectSingleTag();
+      if((reading > -1) && (reading < 5)){
+        vals[reading] = vals[reading] + 1;
+      //debug print vals
+        }
+    }
+        // find max value 
+        int maxIndex = -1;
+        int maxVal = 0;
+        for(int i = 0; i < 5; i++){
+            int curVal = vals[i];
+            if(curVal > maxVal){
+                maxIndex = i;
+                maxVal = curVal;
+            }
+        }
+        tagVal = maxIndex;
     
-    // exited for loop, never correctly received tag reading
-     return -1;
+    // return tagVal is not -1, it is a intended result
+    if(tagVal != -1){
+        return tagVal;
     }
   
-    // decide which reading was gotten most then return it
-  
+  // exited for loop, never correctly received tag reading
+    return -1;
   }
-  
-  // returns a single tag 
-  int DetectSingleTag(){
-    char incomingByte;
-      if(Serial1.available()){
-      Serial1.readBytes(&incomingByte,1);
-      // Serial.println("Message Received: ");
-      Serial.println(incomingByte);
-  
-      if(incomingByte == '0'){
-        //Serial.println("Detecting 0, Place in slot 0");	//debug
-        return 0;
-      }else if(incomingByte == '1'){
-        //Serial.println("Detecting 0, Place in slot 1");	//debug
-        return 1;
-      }else if(incomingByte == '2'){
-        //Serial.println("Detecting 0, Place in slot 2");	//debug
-        return 2;
-      }else if(incomingByte == '3'){
-       // Serial.println("Detecting 0, Place in slot 3");	//debug
-        return 3;
-      }else if(incomingByte == '4'){
-        //Serial.println("Detecting 0, Place in slot 4");	//debug
-        return 4;
-      }else {
-        //Serial.println("Detecting Wall");
-        return 5;
-      }
-      return -1;
+
+  // decide which reading was gotten most then return it
+
+}
+
+// returns a single tag value 
+int DetectSingleTag(){
+  char incomingByte;
+    if(Serial1.available()){
+    Serial1.readBytes(&incomingByte,1);
+    // Serial.println("Message Received: ");
+    Serial.println(incomingByte);
+
+    if(incomingByte == '0'){
+      //Serial.println("Detecting 0, Place in slot 0");	//debug
+      return 0;
+    }else if(incomingByte == '1'){
+      //Serial.println("Detecting 0, Place in slot 1");	//debug
+      return 1;
+    }else if(incomingByte == '2'){
+      //Serial.println("Detecting 0, Place in slot 2");	//debug
+      return 2;
+    }else if(incomingByte == '3'){
+      // Serial.println("Detecting 0, Place in slot 3");	//debug
+      return 3;
+    }else if(incomingByte == '4'){
+      //Serial.println("Detecting 0, Place in slot 4");	//debug
+      return 4;
+    }else {
+      //Serial.println("Detecting Wall");
+      return 5;
     }
+    return -1;
   }
+}
