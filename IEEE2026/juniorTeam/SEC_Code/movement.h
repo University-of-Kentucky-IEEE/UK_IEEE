43fb71aa2e2f3870f287b7d3d8494605967b3c34
dannytincher21-art <dannytincher21@gmail.com>
2026-03-30 17:46:36 -0400

    Uploaded SEC 2026 Code

---
+++ IEEE2026/juniorTeam/SEC_Code/movement.h
@@ -0,0 +1,16 @@
+//
+// Created by Nathan Jones on 10/9/25.
+//
+
+#ifndef IEEE2026_MOVEMENT_H
+#define IEEE2026_MOVEMENT_H
+
+void moveForward();
+void moveBackward();
+void turnLeft();
+void turnRight();
+void strafeLeft();
+void strafeRight();
+void stopMovement();
+
+#endif //IEEE2026_MOVEMENT_H
---
+++ IEEE2026/juniorTeam/SEC_Code/pins.h
@@ -0,0 +1,20 @@
+//
+// Created by Nathan Jones on 10/9/25.
+//
+
+// In reference with https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/
+
+#ifndef IEEE2026_PINS_H
+#define IEEE2026_PINS_H
+
+// Motor A  connections
+#define enableA 9
+#define input1 8
+#define input2 7
+
+// Motor B connections
+#define enableB 3
+#define input3 5
+#define input4 4
+
+#endif //IEEE2026_PINS_H
